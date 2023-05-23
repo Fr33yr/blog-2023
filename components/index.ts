bf7cad92745d3d6b4158654a90f015c68a7e5395
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-05-23 10:14:47 -0300

    barrel update

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as ToggleButton } from './common/buttons/ToggleButton';
 export { default as Footer } from './common/footer/Footer';
 export { default as Navigator } from './common/navigator/Navigator';
+export { default as PostLink } from './postlink/PostLink';
