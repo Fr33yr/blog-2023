e83b58a5818c06ef325925316b0b14bee03b0f2b
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-05-27 11:10:27 -0300

    post models update

--- app/models/post.models.ts
+++ app/models/post.models.ts
@@ -2,6 +2,19 @@ export interface Post {
   title: string;
   content: string;
 }
+
 export interface Message {
   message: string
+}
+
+
+export interface PostType {
+  slug: string
+  title: string
+  date: string
+  coverImage: string
+  author: string
+  excerpt: string
+  ogImageUrl: string
+  content: string
 }
