e659231e282f4997aebac8de10248588a6adb376
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-05-27 11:56:07 -0300

    .

--- app/blog/services/post.services.ts
+++
@@ -1,8 +0,0 @@
-import { Message, Post } from "../../models/index";
-
-export const getPost = (id: string): Promise<Post | Message> => {
-  return fetch("")
-    .then((res) => res.json())
-    .then((res) => res.data())
-    .catch((err: Message) => err);
-};
