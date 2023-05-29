6adc41035ff59746cd196a9bfa7dafe412d04ae0
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-05-29 15:07:47 -0300

    author and post type

--- app/models/post.models.ts
+++ app/models/post.models.ts
@@ -4,17 +4,33 @@ export interface Post {
 }
 
 export interface Message {
-  message: string
+  message: string;
 }
 
+export interface Author {
+  name: string
+  picture: string
+}
 
 export interface PostType {
   slug: string
   title: string
   date: string
   coverImage: string
-  author: string
+  author: Author
   excerpt: string
-  ogImageUrl: string
+  ogImage: {
+    url: string
+  }
   content: string
 }
+
+export type PostInfo = {
+  slug: string;
+  title: string;
+  date: string;
+  coverImage: string;
+  author: string;
+  excerpt: string;
+  ogImageUrl: string;
+};
